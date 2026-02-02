e06d5c36b8b131671c2ba3e4d1749042a58087e9
Susana <hornosu@yahoo.es>
2026-02-02 19:57:53 +0100

    La modal y surrechi

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -5,53 +5,37 @@ function Services() {
         {
             "id" : "0",
             "service": (<>Audit technique</>),
-            "content": (<>
-                            Diagnostic rapide des risques et problèmes potentiels, avec des recommandations pour réduire la dette technique.
-                        </>)
+            "content": (<>Diagnostic rapide des risques et problèmes potentiels, avec des recommandations pour réduire la dette technique.</>)
         }, 
         {
             "id" : "1",
             "service": (<>Conception d’architecture</>),
-            "content": (<>
-                            Recommandations pragmatiques pour mettre en œuvre des architectures qui évoluent douce et organiquement, guidées par le besoin réel plutôt que par la mode.
-                        </>)
+            "content": (<>Recommandations pragmatiques pour mettre en œuvre des architectures qui évoluent douce et organiquement, guidées par le besoin réel plutôt que par la mode.</>)
         }, 
         {
             "id" : "2",
             "service": (<>Développement senior <b>Python (Django)</b> & <b>React</b></>),
-            "content": (<>
-                            Vision systémique des applications, les reliant au besoins du business,
-                            maîtrise de la complexité, capacité de décision. autonomie et fiabilité.
-                        </>)
+            "content": (<>Vision systémique des applications, les reliant au besoins du business, maîtrise de la complexité, capacité de décision. autonomie et fiabilité.</>)
         },
         {
             "id" : "3",
             "service": (<>Reprise et refactoring de code existant</>),
-            "content": (<>
-                            Orienté à projets complexes, en difficulté ou en retard, toujours avec une vision de l’impact business (performance, coûts, délais).</>)
+            "content": (<>Orienté à projets complexes, en difficulté ou en retard, toujours avec une vision de l’impact business (performance, coûts, délais).</>)
         },
         {
             "id" : "4",
             "service": (<>Analyse et remise en maîtrise de systèmes non documentés</>),
-            "content": (<>
-                            Comprendre, documenter et avancer logiciels legacy dont la connaissance s’est perdue avec le temps.</>)
+            "content": (<>Comprendre, documenter et avancer logiciels legacy dont la connaissance s’est perdue avec le temps.</>)
         }, 
         {
             "id" : "5",
             "service": (<>Conception et sécurisation d’API</>),
-            "content": (<>
-                            L’API comme un contrat organisationnel à long terme. L’encapsulation
-                            permet plusieurs développements en parallèle, clarté dans le
-                            développement et vitesse d’évolution du produit avec stabilité à long
-                            terme.
-                        </>)
+            "content": (<>L’API comme un contrat organisationnel à long terme. L’encapsulation permet plusieurs développements en parallèle, clarté dans le développement et vitesse d’évolution du produit avec stabilité à long terme.</>)
         }, 
         {
             "id" : "6",
-            "service": (<>
-                            Mise en œuvre de méthodologies et <i>bons pratiques</i>, <i>Code review</i></>),
-            "content": (<>
-                            Amélioration de la capacité collective à livrer avec fiabilité, prévisibilité et pragmatisme.</>)
+            "service": (<>Mise en œuvre de méthodologies et <i>bons pratiques</i>, <i>Code review</i></>),
+            "content": (<>Amélioration de la capacité collective à livrer avec fiabilité, prévisibilité et pragmatisme.</>)
         },
         {
             "id" : "6",
@@ -62,8 +46,7 @@ function Services() {
         {
             "id" : "7",
             "service": (<>Accompagnement CTO</>),
-            "content": (<>
-                            Amélioration de la capacité collective à livrer avec fiabilité, prévisibilité et pragmatisme.</>)
+            "content": (<>Amélioration de la capacité collective à livrer avec fiabilité, prévisibilité et pragmatisme.</>)
         },
  
     ]
--- src/DRY/MissionModal.jsx
+++ src/DRY/MissionModal.jsx
@@ -11,23 +11,25 @@ const fields=["Mission", "Context", "Issue", "Role", "Result", "Stack"]
             )}*/
 
 return (
-    <>        
-        <div className="missionHead">
-            <img src={mission.image} alt={mission.imageAlt}></img>
-            <div className="infoTitle">
-                <img src={mission.logo} alt={mission.logoAlt} className="photoCenter"></img>
-                {mission.institution}
+    <>  
+        <section className="missionDescription">     
+            <div className="missionHead">
+                <img src={mission.image} alt={mission.imageAlt}></img>
+                <div className="infoTitle">
+                    <img src={mission.logo} alt={mission.logoAlt} className="photoCenter"></img>
+                    <span className="whereAbouts">{mission.institution}</span>
+                </div>
             </div>
-        </div>
-        <div className="missionExplain">
-            <p>{mission.mission}</p>
-            <p>{mission.context}</p>
-            <p>{mission.issue}</p>
-            <p>{mission.role}</p>
-            <p>{mission.stack}</p>
-            <p>{mission.alterTech}</p>
-        </div>            
-        
+            <div className="missionExplain">
+                <p><b>Mission</b><br />{mission.mission}</p>
+                <p><b>Context</b><br />{mission.context}</p>
+                <p><b>Issue</b><br />{mission.issue}</p>
+                <p><b>Rôle</b><br />{mission.role}</p>
+                <p><b>Result</b><br />{mission.result}</p>
+                <p><b>Stack</b><br />{mission.stack}</p>
+                {(mission.alterTech!='') ? <p><b>Autres technologies</b><br />{mission.alterTech}</p>:<p></p>} 
+            </div>            
+        </section> 
     </>
 )
 }
