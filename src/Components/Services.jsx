2d973688c62f68a327973ddecff222663955eaa5
Susana <hornosu@yahoo.es>
2026-02-02 18:41:02 +0100

    Services actualizado

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -6,17 +6,14 @@ function Services() {
             "id" : "0",
             "service": (<>Audit technique</>),
             "content": (<>
-                            Diagnostic rapide des risques et problèmes potentiels, avec des
-                            recommandations pour réduire la dette technique.
+                            Diagnostic rapide des risques et problèmes potentiels, avec des recommandations pour réduire la dette technique.
                         </>)
         }, 
         {
             "id" : "1",
             "service": (<>Conception d’architecture</>),
             "content": (<>
-                            Recommandations pragmatiques pour mettre en œuvre des architectures
-                            qui évoluent douce et organiquement, guidées par le besoin réel plutôt
-                            que par la mode.
+                            Recommandations pragmatiques pour mettre en œuvre des architectures qui évoluent douce et organiquement, guidées par le besoin réel plutôt que par la mode.
                         </>)
         }, 
         {
@@ -31,11 +28,16 @@ function Services() {
             "id" : "3",
             "service": (<>Reprise et refactoring de code existant</>),
             "content": (<>
-                            Orienté à projets complexes, en difficulté ou en retard, toujours avec 
-                            une vision de l’impact business (performance, coûts, délais).</>)
-        }, 
+                            Orienté à projets complexes, en difficulté ou en retard, toujours avec une vision de l’impact business (performance, coûts, délais).</>)
+        },
         {
             "id" : "4",
+            "service": (<>Analyse et remise en maîtrise de systèmes non documentés</>),
+            "content": (<>
+                            Comprendre, documenter et avancer logiciels legacy dont la connaissance s’est perdue avec le temps.</>)
+        }, 
+        {
+            "id" : "5",
             "service": (<>Conception et sécurisation d’API</>),
             "content": (<>
                             L’API comme un contrat organisationnel à long terme. L’encapsulation
@@ -45,28 +47,25 @@ function Services() {
                         </>)
         }, 
         {
-            "id" : "5",
+            "id" : "6",
             "service": (<>
-                            Mise en œuvre de méthodologies et <i>bons pratiques</i>, <i>Code 
-                            review</i></>),
+                            Mise en œuvre de méthodologies et <i>bons pratiques</i>, <i>Code review</i></>),
             "content": (<>
-                            Amélioration de la capacité collective à livrer avec fiabilité, 
-                            prévisibilité et pragmatisme.</>)
+                            Amélioration de la capacité collective à livrer avec fiabilité, prévisibilité et pragmatisme.</>)
         },
         {
             "id" : "6",
             "service": (<>Accompagnement équipes, <i>mentoring</i></>),
             "content": (<>
-                            Emphase en la collectivité, pas les individus ; transmission du
-                            raisonnement, apprentissage sur cas réels.</>)
+                            Emphase en la collectivité, pas les individus ; transmission du raisonnement, apprentissage sur cas réels.</>)
         }, 
         {
             "id" : "7",
             "service": (<>Accompagnement CTO</>),
             "content": (<>
-                            Amélioration de la capacité collective à livrer avec fiabilité, 
-                            prévisibilité et pragmatisme.</>)
-        }, 
+                            Amélioration de la capacité collective à livrer avec fiabilité, prévisibilité et pragmatisme.</>)
+        },
+ 
     ]
     return (
         <>
