c929e085d3905c8bc615ab462cf3427bbb35d2ea
Susana <hornosu@yahoo.es>
2026-02-03 12:45:44 +0100

    fotos slider

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,6 +5,8 @@ function Contact() {
         <>
             <h2>Contactez-nous</h2>
             <h3 className="contactAddress">info@lpgconsulting.fr</h3>
+            <p>17-19 Rue de la Solidarité<br />75019 Paris</p>
+            <h3 className="contactAddress">Tel </h3>
             <ContactForm />
         </>
     )
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,4 +1,10 @@
 import { useEffect, useState } from "react";
+import confiance from "../assets/images/slider/00-nube-logos.avif"; 
+import aglae from "../assets/images/slider/01-aglae.avif";
+import synchrotron from "../assets/images/slider/02-synchrotron.avif";
+import jeanZay from "../assets/images/slider/03-JeanZay.avif";
+import motors from "../assets/images/slider/04-gm.avif";
+import aircraft from "../assets/images/slider/05-aeromexico.avif";
 
 function Slider() {
     const [index, setIndex] = useState(0);
@@ -7,37 +13,37 @@ function Slider() {
         {
             "id" : "0",
             "concept" : "Ils nous ont fait confiance",
-            "source" : "../src/assets/images/slider/00-nube-logos.avif",
+            "source" : confiance,
             "alt": "Logos des entreprise où Luis Arean a fait ses services."
         },
         {
             "id" : "1",
             "concept" : "Automatisation optimale",
-            "source" : "../src/assets/images/slider/01-aglae.avif",
+            "source" : aglae,
             "alt": "AGLAE et positionateur du C2RMF."
         },
         {
             "id" : "2",
             "concept" : "Architecture",
-            "source" : "../src/assets/images/slider/02-synchrotron.avif",
+            "source" : synchrotron,
             "alt": "Synchrotron Soleil à Gif-sur-Yvette."
         },
         {
             "id" : "3",
             "concept" : "Refonte",
-            "source" : "../src/assets/images/slider/03-JeanZay.avif",
+            "source" : jeanZay,
             "alt": "Le supercalculateur dans le centre Idris du CNRS."
         },
         {
             "id" : "4",
             "concept" : "Audit informatique",
-            "source" : "../src/assets/images/slider/04-gm.avif",
+            "source" : motors,
             "alt": "Usine automobile General Motors"
         },
         {
             "id" : "5",
             "concept" : "Reconduction / MVP",
-            "source" : "../src/assets/images/slider/05-aeromexico.avif",
+            "source" : aircraft,
             "alt": "Avion aeromexico survole les montagnes."
         }
     ]
