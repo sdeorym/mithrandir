81ca656b2e12125c46fe9c31b91467c79d80c260
Susana <hornosu@yahoo.es>
2026-02-04 19:24:20 +0100

    Slider logos y wordcloud

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,7 +1,14 @@
+import TextBox from "../DRY/TextBox"
+import Button from "../DRY/Button"
+
 function ContactForm() {
+    
     return (
         <>
-        
+            <div className="contactForm">
+                <TextBox />
+                <Button />
+            </div>
         </>
     )
 }
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,14 +1,18 @@
-/* import { useEffect, useState } from "react";
-import aglae from "../assets/images/slider/01-aglae.avif";
+/* import aglae from "../assets/images/slider/01-aglae.avif";
 import synchrotron from "../assets/images/slider/02-synchrotron.avif";
 import jeanZay from "../assets/images/slider/03-JeanZay.avif";
 import motors from "../assets/images/slider/04-gm.avif";
 import aircraft from "../assets/images/slider/05-aeromexico.avif";*/
-import confiance from "../assets/images/slider/00-nube-logos.avif"; 
-
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import confiance from "../assets/images/slider/00-nube-logos.avif";
+import wordcloud from "../assets/images/slider/01-wordcloud.avif";
+import Button from "../DRY/Button"
 
 function Slider() {
-    /*const [index, setIndex] = useState(0);
+    const [slide, setSlide] = useState(0);
+
 
     const slides = [
         {
@@ -17,6 +21,14 @@ function Slider() {
             "source" : confiance,
             "alt": "Logos des entreprise où Luis Arean a fait ses services."
         },
+        {
+            "id" : "1",
+            "concept" : "",
+            "source" : wordcloud,
+            "alt": "Nuage de compétences qu'on vous offrit."
+        }
+    ]
+        /*
         {
             "id" : "1",
             "concept" : "Automatisation optimale",
@@ -52,12 +64,25 @@ function Slider() {
         setTimeout( () => setIndex(index < (slides.length - 1) ? index+1 : 0), 3000 );}; 
     useEffect(() => { nextCard(); }, [index]);*/
 
+    function goNext() {
+        setSlide(slide === slides.length - 1 ? 0 : slide + 1)
+    }
+
+    function goPrevious() {
+        setSlide(slide === 0 ?   slides.length - 1 : slide - 1 )
+    }
+
+
     return (
         <>
             <div className="slideShow">
+                    <span className="arrows">
+                        <Button onClick={goPrevious} classname={"arrows leftie"} buttonType="button" title={<FontAwesomeIcon icon={ faAngleLeft } alt="<=" size="3x" /> } />
+                        <Button onClick={goNext} classname={"arrows rightie"} buttonType="button" title={<FontAwesomeIcon icon={ faAngleRight } alt="=>" size="3x" />} />
+                    </span>
                 <div className="sliderWindow">
-                    <span>Ils nous ont fait confiance</span>
-                    <img src={confiance} alt="Logos des entreprises où Luis Arean a fait ses services."></img>
+                    <span>{slides[slide].concept}</span>
+                    <img src={slides[slide].source} alt={slides[slide].alt}></img>
                 </div>                        
             </div>            
         </>
