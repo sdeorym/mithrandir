4c4e08383020a2293ef56de246c56c1b7dbd6144
Susana <hornosu@yahoo.es>
2026-02-11 18:23:18 +0100

    cambios en contactform

--- src/DRY/ContactForm.jsx
+++ src/DRY/ContactForm.jsx
@@ -11,62 +11,9 @@ function ContactForm() {
         console.log(formData, { ...formData, [name]: value});
     };
 
-    async function sendMail(e) {
-        e.preventDefault();
-        const from_user="info";
-        const from_domain="lpgconsulting.fr";
-        const to_user="info.contact";
-        const to_domain="peleringris.fr";
-        const url="https://corsproxy.io?https://api.mailjet.com/v3.1/send";
-        const pw="f811be8a3c63505666c60b81f616a030:86d62f30628442442516da94817522c6";
-        const basicAuth = btoa(pw);
-        const token="ZjgxMWJlOGEzYzYzNTA1NjY2YzYwYjgxZjYxNmEwMzA6ODZkNjJmMzA2Mjg0NDI0NDI1MTZkYTk0ODE3NTIyYzY=";
-        const dataToSend={
-            'Messages':[
-                {
-                    'From':{
-                        'Email': from_user+"@"+from_domain,
-                        'Name': 'LPG Consulting Webpage'
-                    },
-                    'To': 
-                        [
-                            {
-                                'Email':to_user + "@" + to_domain,
-                                'Name': 'LPG Consulting Webpage'
-                            }
-                        ],
-                    'Subject': '[Webpage contact]',
-                    'TextPart': 
-                        {
-                            'Name': "Chomosgón",
-                            'Email': "chomosgon@wapakoneta.us",
-                            'Message': "Uagadugu no se rinde"
-                        }
-                }
-            ]
-        };
-        const x=JSON.stringify(dataToSend);
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Basic ${basicAuth}`
-                },
-                body: JSON.stringify(dataToSend)
-            });
-            if (!response.ok) {
-                throw new Error("Il y a eu une erreur.")
-            }
-            const result = response.json();
-            console.log('Form data submitted:', result);
-            }
-        catch(error) {console.error('Error:', error)}
-    };
-
     return (
         <>
-            <form className="contactForm">
+            <form className="contactForm" method="post" action="https://api.lpgconsulting.fr/send-email">
                 <div className="textbox">
                     <label htmlFor="username">Nom</label>
                     <input type="name" id="username" name="username" onBlur={handleChange} required />
@@ -83,10 +30,9 @@ function ContactForm() {
                     buttonType="submit" 
                     title="Envoyer" 
                     value="Send" 
-                    disabled = {((formData.username=="") || (formData.email=="") /*|| (formData.message=="")*/) ? true : false} 
-                    classname={((formData.username!="") && (formData.email!="") /*&& (formData.message!="")*/) ? "submit enabled" : "submit disabled"} 
-                    data={formData}
-                    onClick={sendMail} />                
+                    disabled = {((formData.username=="") || (formData.email=="")) ? true : false} 
+                    classname={((formData.username!="") && (formData.email!="")) ? "submit enabled" : "submit disabled"} 
+                    data={formData} />                
             </form>
         </>
     )
